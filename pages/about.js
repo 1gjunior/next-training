9e381889289eb2fc5ca2e9682e07a84357d61105
Gilberto Junior <junior.siilva182@gmail.com>
2021-06-21 19:50:51 -0300

    feat: added about page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>WebDev News</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
