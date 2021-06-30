0ed09fd89a48f63b0f3e84513c849c41a4ea9a37
Gilberto Junior <junior.siilva182@gmail.com>
2021-06-30 16:15:51 -0300

    feat: added getStaticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -16,7 +16,7 @@ const article = ({ article }) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   );
@@ -27,6 +27,19 @@ export const getServerSideProps = async (context) => {
     props: {
       article,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
   };
 };
 
