73514d172a5f523b178753e7f2f90e852c31a324
Gilberto Junior <junior.siilva182@gmail.com>
2021-06-30 15:55:27 -0300

    feat: added ArticleItem component

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,11 @@
+import ArticleItem from './ArticleItem';
 import articleStyles from '../styles/Article.module.css';
 
 const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-        <h3 key={article.id}>{article.title}</h3>
+        <ArticleItem key={article.id} article={article} />
       ))}
     </div>
   );
